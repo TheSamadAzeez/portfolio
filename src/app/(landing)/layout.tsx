bacd42785b91f5067d1ed874ea64bc91f12d7e00
TheSamadAzeez <azeezsamad2004@gmail.com>
2025-11-04 13:38:58 +0100

    refactor: simplify LandingLayout component structure and remove unnecessary section wrapper

--- src/app/(landing)/layout.tsx
+++ src/app/(landing)/layout.tsx
@@ -2,8 +2,8 @@ import { Dock } from '@/components/custom/dock'
 
 export default function LandingLayout({ children }: { children: React.ReactNode }) {
   return (
-    <main className="h-screen w-screen bg-gray-500">
-      <section className="w-full"> {children}</section>
+    <main className="w-screen">
+      {children}
       <Dock />
     </main>
   )
