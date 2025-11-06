33e904dc191fb53c8d09058a9d5f66c0669fe5da
TheSamadAzeez <azeezsamad2004@gmail.com>
2025-11-06 15:57:35 +0100

    feat: add "See more" button to FeaturedProjects component for enhanced navigation

--- src/app/components/featured-project.tsx
+++ src/app/components/featured-project.tsx
@@ -2,6 +2,9 @@
 import React from 'react'
 import { StickyScroll } from '@/components/ui/sticky-scroll-reveal'
 import { portfolioData } from '../../../utils/data'
+import Link from 'next/link'
+import { IconArrowNarrowRight } from '@tabler/icons-react'
+import { Button } from '@/components/ui/button'
 
 export default function FeaturedProjects() {
   return (
@@ -13,6 +16,17 @@ export default function FeaturedProjects() {
 
       <div className="w-full">
         <StickyScroll content={portfolioData.featuredProjects} contentClassName="h-70 w-90" />
+      </div>
+      <div className="mt-8 flex w-full items-center justify-center">
+        <Button
+          asChild
+          className="group bg-muted-foreground/80 hover:bg-muted-foreground animate-pulse border-none hover:scale-105"
+        >
+          <Link href={'/projects'} className="flex items-center justify-center gap-2 rounded-full px-6 py-2">
+            <span className="text-lg font-medium text-white transition-all group-hover:scale-105">See more</span>
+            <IconArrowNarrowRight className="h-5 w-5 text-white transition-transform group-hover:translate-x-1" />
+          </Link>
+        </Button>
       </div>
     </section>
   )
