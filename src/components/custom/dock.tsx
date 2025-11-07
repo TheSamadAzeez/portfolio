fd6a393814c327951721444cdc4dadc04fe63085
TheSamadAzeez <azeezsamad2004@gmail.com>
2025-11-07 17:02:15 +0100

    refactor: simplify Dock component layout by removing unnecessary wrapper div

--- src/components/custom/dock.tsx
+++ src/components/custom/dock.tsx
@@ -72,12 +72,10 @@ export function Dock() {
   }
 
   return (
-    <div className="flex h-[35rem] w-full items-center justify-center">
-      <FloatingDock
-        items={links}
-        desktopClassName="fixed bottom-3 z-100 backdrop-blur-xl bg-white/10 dark:bg-black/10 border border-white/20 dark:border-white/10 rounded-3xl shadow-xl"
-        mobileClassName="fixed bottom-2 z-100 right-1"
-      />
-    </div>
+    <FloatingDock
+      items={links}
+      desktopClassName="fixed bottom-3 left-1/2 transform -translate-x-1/2 z-[9999] backdrop-blur-xl bg-white/10 dark:bg-black/10 border border-white/20 dark:border-white/10 rounded-3xl shadow-xl"
+      mobileClassName="fixed bottom-2 right-2 z-[9999]"
+    />
   )
 }
