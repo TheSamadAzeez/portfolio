9b57730d05e97a4d553e52b03469f6934ccf85c9
TheSamadAzeez <azeezsamad2004@gmail.com>
2025-11-14 19:59:47 +0100

    feat: add MobileNavigation component to LandingLayout for improved navigation

--- src/app/(landing)/layout.tsx
+++ src/app/(landing)/layout.tsx
@@ -1,8 +1,10 @@
 import { Dock } from '@/components/custom/dock'
+import MobileNavigation from '@/components/custom/mobile-navigation'
 
 export default function LandingLayout({ children }: { children: React.ReactNode }) {
   return (
     <main className="w-screen">
+      <MobileNavigation />
       {children}
       <Dock />
     </main>
